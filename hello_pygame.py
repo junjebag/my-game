2a4ad901ff2ad4d630f93119d0bfa85590a1b821
junjebag <20260152@vision.hoseo.edu>
2026-03-22 22:09:19 +0900

    방향키로 원 움직이는 기능 추가

---
+++ hello_pygame.py
@@ -0,0 +1,37 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("My First Pygame")
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+clock = pygame.time.Clock()
+running = True
+
+x, y = 400, 300  # 원의 초기 위치
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= 5
+    if keys[pygame.K_RIGHT]:
+        x += 5
+    if keys[pygame.K_UP]:
+        y -= 5
+    if keys[pygame.K_DOWN]:
+        y += 5
+
+    screen.fill(WHITE)
+    pygame.draw.circle(screen, BLUE, (x, y), 50)
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
